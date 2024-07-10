c90226fbabc690adfd8db4850e2fc3017a2773d0
animesh65432 <kiranduttta234@gmail.com>
2024-07-10 13:25:28 +0530

    setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import store from "./store/index.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
